4ecd698d9ddae3f3eff5bdc0848f1caaa4d3e6e6
Thanh Cong Nguyen <93239449+Thanh-Cong0602@users.noreply.github.com>
2026-01-08 15:18:20 +0700

    [Lecture 6] Unit Test for Core Middleware: Async Handler

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -1,5 +1,3 @@
-/** Author: @trungquandev (TrungQuanDev - Một Lập Trình Viên) - https://youtube.com/@trungquandev */
-
 import { Request, Response } from 'express'
 
 import { UserService } from '~/modules/users/user.service'
