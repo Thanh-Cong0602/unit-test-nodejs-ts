0fcd7da91be11c25cb37eb0f419d9a645d14d92e
Thanh Cong Nguyen <93239449+Thanh-Cong0602@users.noreply.github.com>
2026-01-20 11:21:36 +0700

    [Lecture 11] Unit Test for CORS Configuration File

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,5 +1,3 @@
-/** Author: @trungquandev (TrungQuanDev - Một Lập Trình Viên) - https://youtube.com/@trungquandev */
-
 import cors, { CorsOptions } from 'cors'
 
 import { env } from '~/config/env'
@@ -10,9 +8,11 @@ const allowedOrigins = env.CORS_ORIGINS.split(',').map(o => o.trim())
 const corsOptions: CorsOptions = {
   origin(origin, callback) {
     // Cho phép nếu không có origin (ví dụ Postman, curl)
-    if (!origin) return callback(null, true)
+    if (!origin && ['development', 'test', 'staging'].includes(env.NODE_ENV)) {
+      return callback(null, true)
+    }
 
-    if (allowedOrigins.includes(origin)) {
+    if (allowedOrigins.includes(origin as string)) {
       return callback(null, true)
     }
     // Nếu domain không được phép
