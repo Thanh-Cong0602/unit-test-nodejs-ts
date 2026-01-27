1877063f4b3901b1965c4a374aa8044086cd5c5e
Thanh Cong Nguyen <93239449+Thanh-Cong0602@users.noreply.github.com>
2026-01-27 15:49:13 +0700

    [Lecture 15] Unit Test for the Controller Layer

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -7,11 +7,15 @@ export const UserController = {
   register: async (req: Request, res: Response) => {
     const { email, username, password } = req.body
     const user = await UserService.register(email, username, password)
-    return res.status(StatusCodes.CREATED).json({ id: user._id, email: user.email, username: user.username })
+    return res.status(StatusCodes.CREATED).json({
+      _id: user._id,
+      email: user.email,
+      username: user.username
+    })
   },
 
   list: async (_req: Request, res: Response) => {
     const users = await UserService.list()
-    res.json(users)
+    res.status(StatusCodes.OK).json(users)
   },
 }
